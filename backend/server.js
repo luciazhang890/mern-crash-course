0333e15bd3c06fe61fecd8b9bbc704b3183a4228
Lucy Zhang <luciazhang890@gmail.com>
2025-03-30 01:00:15 +1030

    add some content in building API

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,30 @@
 import express from 'express';
 import dotenv from "dotenv";
 import {connectDB} from './config/db.js';
+import Product from './models/productModel.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {
-    res.send('Server is ready123');
+app.post('/api/products', async (req, res) => {
+    const product = req.body; //user will send this data 
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({success:false, message: 'Please provide all fields' });
+    }
+
+    const newProduct = new Product(product)
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true, data: newProduct});
+    } catch (error) {
+        console.error("Error in creating product:", error.message);
+        res.status(500).json({success:false, message: 'Server error'});
+    }
 });
 
-console.log(process.env.MONGO_URI);
+//Postman desktop app
 
 app.listen(5000, () => {
     connectDB();
