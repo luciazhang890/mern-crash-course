a75a6c756500411164e1a645fc3da5defeb14080
Lucy Zhang <luciazhang890@gmail.com>
2025-03-29 14:56:13 +1030

    set up mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 //const express = require('express');
 
 import express from 'express';
+import dotenv from "dotenv";
+import {connectDB} from './config/db.js';
+
+dotenv.config();
+
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/products', (req, res) => {
     res.send('Server is ready123');
 });
 
+console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000 ");
 });
+
+//Ziq2WfMLsTuR7qmh
