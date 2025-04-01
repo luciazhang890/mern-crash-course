75331d81c125a8dbb45eef68f90bc0327c483947
Lucy Zhang <luciazhang890@gmail.com>
2025-04-01 20:18:15 +1030

    frontend with some problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue} from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+
 import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
 import NavBar from "./components/Navbar";
+import React from 'react'; //add additionally
 
 function App() {
   
 
   return (
-    <Box minH="100vh" bg="gray.100">
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
        <NavBar /> 
       <Routes>
         <Route path="/" element={<HomePage />} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider } from "@/chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from 'react-router-dom';
+//ReactDOM.Render(<App />, document.getElementById('root'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <ChakraProvider>
         <App />
-      </BrowserRouter>
+      </ChakraProvider>
+    </BrowserRouter>
       
-    </ChakraProvider>
+    
   </React.StrictMode>
 )
