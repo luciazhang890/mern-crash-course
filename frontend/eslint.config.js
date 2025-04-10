98c6baab15bd87b3246ab3004d9dff1eb1a1dc36
Lucy Zhang <luciazhang890@gmail.com>
2025-04-10 18:50:46 +0930

    add homepage with bugs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -21,6 +21,8 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      "react/prop-types": "off",
+      //'react-refresh/only-export-components': 'off',
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
